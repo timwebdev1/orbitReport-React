2cebe275a78613f7f37eaffef79717883aa55e22
Timothy McDermott <tlmcdermott79@gmail.com>
2024-08-29 10:16:02 -0500

    table display completed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,26 @@
 const Table = ({ sat }) => {
+
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map((data, id) => (
+          <tr key={id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{`${(data.operational === true) ? "active" : "inactive"}`}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
